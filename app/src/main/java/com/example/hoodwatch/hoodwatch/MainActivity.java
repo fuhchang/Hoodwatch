04f761296ddd87d3939b07a708a05fbb46122f46
fuh_c <fuhchang123@gmail.com>
2016-11-19 21:36:15 +0800

    add google map

--- app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
+++ app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.hoodwatch.hoodwatch;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello world12 ");
+        Fragment fragment = Fragment.instantiate(this, eventLocFragment.class.getName());
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.container, fragment);
+        ft.commit();
     }
 }
