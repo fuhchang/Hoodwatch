1c0b08c924df4be5387c65d81b199f99d65459ff
NormanTJL <NormanTJL91@gmail.com>
2016-11-23 17:26:05 +0800

    List of items using cardview and linearLayout but need to solve issue on the arrangement of the textview and imageview in the cardview

--- app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
+++ app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
@@ -1,22 +1,61 @@
 package com.example.hoodwatch.hoodwatch;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.style.BackgroundColorSpan;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-    ListView list;
-    String[] web = {"Google Plus", "Twitter", "Windows", "Bing", "Itunes", "Wordpress", "Drupal"} ;
-    Integer[] imageId = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5, R.drawable.image6, R.drawable.image7};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomList adapter = new CustomList(MainActivity.this, web, imageId);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        lp.width = LinearLayout.LayoutParams.MATCH_PARENT;
+        lp.height = 350;
+        LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp1.width=LinearLayout.LayoutParams.MATCH_PARENT;
+        lp1.height=700;
+
+        for(int i=0;i<20;i++){
+            TextView tv = new TextView(this);
+            ImageView iv = new ImageView(this);
+            iv.setImageResource(R.drawable.image1);
+            tv.setText("hello");
+            CardView cv = new CardView(this);
+            lp.weight=1;
+
+            tv.setLayoutParams(lp);
+            lp.gravity = Gravity.END;
+            iv.setLayoutParams(lp);
+            cv.addView(iv,0);
+            cv.addView(tv, 1);
+            cv.setLayoutParams(lp1);
+            ll.addView(cv, i);
+        }
+
+
+       /* CustomList adapter = new CustomList(MainActivity.this, web, imageId);
         list=(ListView)findViewById(R.id.list);
         list.setAdapter(adapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -26,6 +65,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
 
             }
-        });
+        });*/
     }
 }
