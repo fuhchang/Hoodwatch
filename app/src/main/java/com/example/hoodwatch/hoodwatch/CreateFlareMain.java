6bcb7f37d8292d890481027d04d6a619fee04691
fuh_c <fuhchang123@gmail.com>
2016-11-27 13:27:46 +0800

    fix camera issue and add send button on keyboard

--- app/src/main/java/com/example/hoodwatch/hoodwatch/CreateFlareMain.java
+++ app/src/main/java/com/example/hoodwatch/hoodwatch/CreateFlareMain.java
@@ -1,9 +1,13 @@
 package com.example.hoodwatch.hoodwatch;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
 import android.net.Uri;
 import android.os.Environment;
@@ -13,20 +17,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.beardedhen.androidbootstrap.BootstrapCircleThumbnail;
 import com.beardedhen.androidbootstrap.TypefaceProvider;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import static android.R.attr.bitmap;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class CreateFlareMain extends AppCompatActivity {
     ImageView im;
+    String path;
+    Uri imageUri;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +51,20 @@ public class CreateFlareMain extends AppCompatActivity {
         String type = bundle.getString("type");
         LinearLayout inputLayout = (LinearLayout)findViewById(R.id.input);
         inputLayout.bringToFront();
-
         im  = (ImageView) findViewById(R.id.upload);
         im.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+                if (ActivityCompat.checkSelfPermission(CreateFlareMain.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                   ActivityCompat.requestPermissions(CreateFlareMain.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},0);
+                }
+                imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                 }else{
@@ -50,18 +72,83 @@ public class CreateFlareMain extends AppCompatActivity {
                 }
             }
         });
-
+        BootstrapCircleThumbnail thumb = (BootstrapCircleThumbnail) findViewById(R.id.thumb);
+        thumb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CreateFlareMain.this,"SUBMIT",Toast.LENGTH_SHORT);
+                Log.d("submit" , "submitting1");
+            }
+        });
+        EditText editText = (EditText) findViewById(R.id.msg);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("submit" , "submitting2");
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            im.setImageBitmap(imageBitmap);
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //im.setImageBitmap(imageBitmap);
+            Bitmap imageBitmap= null;
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), imageUri);
+                saveToInternalStorage(imageBitmap, "Image");
+                loadImageFromStorage(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
+    private String saveToInternalStorage(Bitmap bitmapImage, String imgName){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        path = directory.getPath();
+        File mypath=new File(directory,imgName+".jpg");
 
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            Log.d("image", "The path is " + path);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    private void loadImageFromStorage(String path)
+    {
+
+        try {
+            File f=new File(path, "Image.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            ImageView img=(ImageView)findViewById(R.id.upload);
+            img.setImageBitmap(b);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
 }
