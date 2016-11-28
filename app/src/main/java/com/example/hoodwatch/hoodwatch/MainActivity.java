73cd9da581c1d6bd174a1586e8fb1fa96380cca6
fuhchang <fuhchang123@gmail.com>
2016-11-28 15:36:36 +0800

    fix

--- app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
+++ app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TypefaceProvider.registerDefaultIconSets();
-        Intent intent = new Intent(this, CreateFlareType.class);
-        startActivity(intent);
+
     }
 }
