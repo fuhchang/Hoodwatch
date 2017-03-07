a2bf86acfc641a72b43b4dda53dd6ec9dca5cda7
fuhchang <fuhchang123@gmail.com>
2017-03-07 14:36:47 +0800

    update and fix bug

--- app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
+++ app/src/main/java/com/example/hoodwatch/hoodwatch/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends Activity {
     GoogleApiClient googleApiClient = null;
 
     private DatabaseReference mPostReference;
-    private StorageReference mStorageRef;
     int maxSize =0;
     ArrayList<Geofence> geoList = new ArrayList<>();
     ArrayList<Flare> listofFlares = new ArrayList<>();
@@ -94,7 +93,6 @@ public class MainActivity extends Activity {
         rv = (RecyclerView) findViewById(R.id.rv_main);
         rv.setHasFixedSize(false);
         mPostReference = FirebaseDatabase.getInstance().getReference();
-        mStorageRef = FirebaseStorage.getInstance().getReference();
         adapter = new flareAdapter(this, listofFlares);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         rv.setLayoutManager(mLayoutManager);
